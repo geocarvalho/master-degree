4d761d94dd300f34a67b0bafe302a9feb6799c58
George Carvalho <geocarvalho91@gmail.com>
2020-10-21 01:19:58 -0300

    create transposed csvs

--- merge_gse109381.py
+++ merge_gse109381.py
@@ -6,19 +6,25 @@ csv2 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part2.csv"
 csv3 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part3.csv"
 csv4 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part4.csv"
 
-gse1_df = pd.read_csv(csv1, index_col=0, header=None).transpose()
-gse2_df = pd.read_csv(csv2, index_col=0, header=None).transpose()
-gse3_df = pd.read_csv(csv3, index_col=0, header=None).transpose()
-gse4_df = pd.read_csv(csv4, index_col=0, header=None).transpose()
+# Create transposed file
+csv_cols_list = {}
+for csv in [csv2, csv3, csv4]:
+    mylst = []
+    for chunk in pd.read_csv(csv, index_col=0, header=None, low_memory=False, chunksize=100000):
+        mylst.append(chunk)
+    gse_df = pd.concat(mylst, axis=0)
+    del mylst
+    gse_df = gse_df.transpose()
+    gse_df.rename(columns={np.NaN: "samples"}, inplace=True)
+    csv_cols_list[csv] = gse_df.columns
+    csv_output_name = csv.replace(".csv", "_t.csv")
+    gse_df.to_csv(csv_output_name, index=False)
 
-gse1_df.rename(columns={np.NaN: "samples"}, inplace=True)
-gse2_df.rename(columns={np.NaN: "samples"}, inplace=True)
-gse3_df.rename(columns={np.NaN: "samples"}, inplace=True)
-gse4_df.rename(columns={np.NaN: "samples"}, inplace=True)
+print(csv_cols_list)
+# print("Take common features") 
+# comm_cpgs1 = list(set(list(gse1_df.columns)).intersection(list(gse2_df.columns)))
+# comm_cpgs2 = list(set(list(gse3_df.columns)).intersection(list(gse4_df.columns)))
+# comm_cpgs = comm_cpgs1.intersection(comm_cpgs2)
 
-comm_cpgs1 = list(set(list(gse1_df.columns)).intersection(list(gse2_df.columns)))
-comm_cpgs2 = list(set(list(gse3_df.columns)).intersection(list(gse4_df.columns)))
-comm_cpgs = comm_cpgs1.intersection(comm_cpgs2)
-
-print(comm_cpgs)
-print(len(comm_cpgs2))
+# print(comm_cpgs)
+# print(len(comm_cpgs2))
