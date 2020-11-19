179fd9112f0d120c0676d5d9b8f85a1d6c3c805d
George Carvalho <geocarvalho91@gmail.com>
2020-11-19 07:58:42 -0300

    SSDP runned for glioblastome samples

--- merge_gse109381.py
+++ merge_gse109381.py
@@ -10,12 +10,12 @@ def normalize_values(df):
     return df
 
 
-csv1 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part1.csv"
-csv2 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part2.csv"
-csv3 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part3.csv"
-csv4 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part4.csv"
-pheno = "/home/watson/george/master-degree/GSE109381/GSE109381_all_phenotype_filtered_tclasses.csv"
-to_transpose = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_formatted.csv"
+# csv1 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part1.csv"
+# csv2 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part2.csv"
+# csv3 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part3.csv"
+# csv4 = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_part4.csv"
+# pheno = "/home/watson/george/master-degree/GSE109381/GSE109381_all_phenotype_filtered_tclasses.csv"
+# to_transpose = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_formatted.csv"
 # Create transposed file
 # csv_cols_list = {}
 # for csv in [csv1, csv2, csv3, csv4]:
@@ -72,11 +72,51 @@ to_transpose = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_fo
 #           out.write(f.read())
 
 # Create transposed file
+# mylst = []
+# for chunk in pd.read_csv(to_transpose, index_col=0, header=None, low_memory=False, chunksize=100000):
+#     mylst.append(chunk)
+#     gse_df = pd.concat(mylst, axis=0)
+# del mylst
+# gse_df = gse_df.transpose()
+# csv_output_name = to_transpose.replace(".csv", "_t.csv")
+# gse_df.to_csv(csv_output_name, index=False)
+
+# Work with the most common class
+gse = "/home/watson/george/master-degree/GSE109381/GSE109381_bvalues_gbm.csv"
+csv = "/home/watson/george/master-degree/GSE109381/GBM_class_sample_2cols.csv"
+# csv
+csv_df = pd.read_csv(csv, names=["samples", "class"])
+csv_df["class"] = pd.Categorical(csv_df["class"])
+csv_df["class_num"] = csv_df["class"].cat.codes
+csv_df.to_csv("/home/watson/george/master-degree/GSE109381/GBM_class_sample_categories.csv", index=False)
+classes = csv_df[["samples", "class_num"]]
+
+# gse
+gse_df = pd.read_csv(gse, index_col=0, header=None).transpose()
+gse_df.rename(columns={np.NaN: "samples"}, inplace=True)
+gse_df["samples"] = [sample.split("_")[0] for sample in gse_df["samples"]]
+# the majority class is "GBM, RTK II" that now is 5
+
+# merge
+merged_df = gse_df.merge(classes, on="sample")
+merged_df = merged_df.set_index("samples")
+cols = gse_df.columns.to_list()
+gse_df[cols] = gse_df[cols].astype(np.float32)
+gse_df = normalize_values(gse_df)
+
 mylst = []
-for chunk in pd.read_csv(to_transpose, index_col=0, header=None, low_memory=False, chunksize=100000):
+for chunk in pd.read_csv(csv, low_memory=False, chunksize=100000):
+    cols = [col for col in chunk.columns if col != "class_num"]
+    chunk[cols] = chunk[cols].astype(float)
+    chunk[chunk[cols] <= 0.2] = 0
+    chunk[chunk[cols] >= 0.8] = 2
+    chunk[(chunk[cols] > 0.2) & (chunk[cols] < 0.8)] = 1
     mylst.append(chunk)
-    gse_df = pd.concat(mylst, axis=0)
-del mylst
-gse_df = gse_df.transpose()
-csv_output_name = to_transpose.replace(".csv", "_t.csv")
-gse_df.to_csv(csv_output_name, index=False)
+gse_df = pd.concat(mylst, axis=0)
+gse_df.to_csv("GSE109381_bvalues_gbm_merged_classes_formatted.csv", index=False)
+
+# Remove float points to have just integers
+# cat GSE109381_bvalues_gbm_merged_classes_formatted.csv | sed 's/\.0//g' > GSE109381_bvalues_gbm_merged_classes_formatted_nopoints.csv
+
+# Rodar o SSDP+
+# java -jar -Xmx50G -XX:+UseConcMarkSweepGC ssdp_plus/out/artifacts/ssdp_plus_jar/ssdp_plus.jar master-degree/GSE109381_bvalues_gbm_merged_classes_formatted_nopoints.csv 5 0.9 5 | tee -a  master-degree/GSE109381_log.txt
--- run_gse109381.R
+++ run_gse109381.R
@@ -5,16 +5,49 @@ library(IlluminaHumanMethylation450kmanifest)
 
 sample <- "GSE109381"
 
-idat_file <- paste(sample, "/", "idat/part4", sep="")
+idat_file <- paste(sample, "/", "idat/major", sep="")
 
 rgSet <- read.metharray.exp(idat_file)
 gc()
-grSet <- preprocessQuantile(rgSet)
+# grSet <- preprocessQuantile(rgSet)
+
+# Differential methylation analysis
+# calculate the detection p-values
+detP <- detectionP(rgSet)
+# remove poor quality samples
+keep <- colMeans(detP) < 0.05
+rgSet <- rgSet[,keep]
+# remove poor quality samples from detection p-value table
+detP <- detP[,keep]
+# normalize the data; this results in a GenomicRatioSet object
+mSetSq <- preprocessQuantile(rgSet) # 485512
+# preprocessQuantile() is more suited for datasets where you do not expect 
+# global differences between your samples, for example a single tissue
+# ensure probes are in the same order in the mSetSq and detP objects
+detP <- detP[match(featureNames(mSetSq),rownames(detP)),]
+# remove any probes that have failed in one or more samples
+keep <- rowSums(detP < 0.01) == ncol(mSetSq)
+# table(keep) # 21821 removed
+mSetSqFlt <- mSetSq[keep,]
+# if your data includes males and females, remove probes on the sex chromosomes
+ann450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+keep <- !(featureNames(mSetSqFlt) %in% ann450k$Name[ann450k$chr %in% 
+                                                        c("chrX","chrY")])
+# table(keep) 
+mSetSqFlt <- mSetSqFlt[keep,]
+# remove probes with SNPs at CpG site
+mSetSqFlt <- dropLociWithSnps(mSetSqFlt) # 402974
+
+# exclude cross reactive probes 
+csv_file <- "/home/watson/george/master-degree/48639-non-specific-probes-Illumina450k_part1.csv"
+xReactiveProbes <- read.csv(csv_file, stringsAsFactors=FALSE)
+keep <- !(featureNames(mSetSqFlt) %in% xReactiveProbes$TargetID)
+mSetSqFlt <- mSetSqFlt[keep,] # 377799
+bVals <- getBeta(mSetSqFlt)
 
 # Download beta-values
-bvalues <- paste(sample, "/", sample, "_bvalues_part4.csv", sep="")
-beta <- getBeta(grSet)
-write.csv(file=bvalues, x=beta)
+bvalues <- paste(sample, "/", sample, "_bvalues_gbm.csv", sep="")
+write.csv(file=bvalues, x=bVals)
 
 # Download phenotype data
 pheno <- paste(sample, "/", sample, "_all_phenotype.csv", sep="")
