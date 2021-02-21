4820d2dcdd95dd5981c26084be5bc6f73fc08be6
George Carvalho <geocarvalho91@gmail.com>
2021-02-21 16:56:58 -0300

    Download GSE stable in server

--- download_geo/run_docker.sh
+++ download_geo/run_docker.sh
@@ -1,5 +1,10 @@
+# Download dataset
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE109nnn/GSE109381/suppl//GSE109381_RAW.tar
+
+# Build and run docker
 docker build -t master-degree/download_geo .
 docker container run --rm -it  -v $(pwd):/download_geo  master-degree/download_geo
 
 # https://www.bioconductor.org/help/docker/
 # docker run -it -e PASSWORD=bioc -v $(pwd):/download_geo bioconductor/bioconductor_docker:devel bash
+
