20a32982ef217a7b512b92a5bf2890e33a29b652
George Carvalho <george@genomika.com.br>
2021-03-20 18:26:59 -0300

    differential methylation done

--- download_geo/differential_meth.R
+++ download_geo/differential_meth.R
@@ -106,9 +106,43 @@ save(mVals, file="mVals.Rda")
 load("design.Rda")
 load("mVals.Rda")
 # fit the linear model 
-fit <- lmFit(mVals, design)
-fit <- eBayes(fit)
+# fit <- lmFit(mVals, design)
+# fit <- eBayes(fit)
 
 # The numbers of hyper-methylated (1) and hypo-methylated (-1)
-summary(decideTests(fit))
-save(fit, file="fit.Rda")
+# summary(decideTests(fit))
+# save(fit, file="fit.Rda")
+load("fit.Rda")
+
+# create a contrast matrix for specific comparisons
+contMatrix <- makeContrasts("C50-normal", levels=design)
+
+# fit the contrasts
+fit2 <- contrasts.fit(fit, contMatrix)
+fit2 <- eBayes(fit2)
+# look at the numbers of DM CpGs at FDR < 0.05
+summary(decideTests(fit2)) # 175827
+diff <- decideTests(fit2)
+write.table(diff, file="diff_probes.csv", sep=",", row.names=TRUE)
+
+# get the table of results for the first contrast (naive - rTreg)
+ann450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+ann450kSub <- ann450k[match(rownames(mVals),ann450k$Name),
+                      c(1:4,12:19,24:ncol(ann450k))]
+DMPs <- topTable(fit2, num=Inf, coef=1, genelist=ann450kSub)
+write.table(DMPs, file="DMPs.csv", sep=",", row.names=FALSE)
+
+# Get the significant CpG sites at less than 5% FDR
+sigCpGs <- DMPs$Name[DMPs$adj.P.Val<0.05]
+# length(sigCpGs) 175827
+# Write a txt file with the significant CpGs
+fileConn <- file("sigCpGs.txt")    
+writeLines(sigCpGs, fileConn)    
+close(fileConn)
+
+# Get all the CpG sites used in the analysis to form the background
+all <- DMPs$Name
+gst <- gometh(sig.cpg=sigCpGs, all.cpg=all, plot.bias=TRUE)
+
+# Top 10 GO categories
+topGSA(gst, number=10)
