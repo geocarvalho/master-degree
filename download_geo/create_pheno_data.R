20054be80b4bcffce6291de7372b31200229c3a9
George Carvalho <geocarvalho91@gmail.com>
2021-07-28 22:51:44 +0400

    Move gz to txt

--- download_geo/create_pheno_data.R
+++ download_geo/create_pheno_data.R
@@ -6,6 +6,6 @@ sample <- "GSE51032"
 pheno <- paste(sample, "/", sample, "_all_phenotype.csv", sep="")
 # Download Matrix
 # geoMat <- getGEO(sample) # timeout, use download_GSE51032.sh
-geoMat <- getGEO(filename="./GSE51032/GSE51032_series_matrix.txt.gz", GSEMatrix=TRUE)
+geoMat <- getGEO(filename="./GSE51032/GSE51032_series_matrix.txt", GSEMatrix=TRUE)
 pD.all <- pData(geoMat)
 write.csv(file=pheno, x=pD.all)
