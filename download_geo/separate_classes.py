a420df366cc5a2d36c223c048a03ba148c16394d
George Carvalho <geocarvalho91@gmail.com>
2021-07-30 18:49:52 +0400

    correct path

--- download_geo/separate_classes.py
+++ download_geo/separate_classes.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 
-pheno = "/home/watson/george/master-degree/download_geo/GSE51032/GSE51032_all_phenotype.csv"
+pheno = "GSE51032/GSE51032_all_phenotype.csv"
 pheno_df = pd.read_csv(pheno)
-direc = os.path.dirname(pheno)
+direc = os.path.dirname(os.path.abspath(pheno))
 
 pheno_df["cancer type (icd-10):ch1"] = pheno_df["cancer type (icd-10):ch1"].replace(
     np.nan, "normal", regex=True)
@@ -14,7 +14,9 @@ pheno_df.to_csv(
 
 pheno_df[pheno_df["cancer type (icd-10):ch1"]=="normal"]["Unnamed: 0"].to_csv(
     os.path.join(direc, "GSE51032_all_phenotype_onlynormals.csv"), index=False, header=False)
+# breast cancer
 pheno_df[pheno_df["cancer type (icd-10):ch1"]=="C50"]["Unnamed: 0"].to_csv(
     os.path.join(direc, "GSE51032_all_phenotype_onlymama.csv"), index=False, header=False)
-pheno_df[pheno_df["cancer type (icd-10):ch1"]=="C18"]["Unnamed: 0"].to_csv(
-    os.path.join(direc, "GSE51032_all_phenotype_onlycolon.csv"), index=False, header=False)
+# colon cancer
+# pheno_df[pheno_df["cancer type (icd-10):ch1"]=="C18"]["Unnamed: 0"].to_csv(
+#     os.path.join(direc, "GSE51032_all_phenotype_onlycolon.csv"), index=False, header=False)
