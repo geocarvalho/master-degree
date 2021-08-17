27106841ac8b1c501ab0bf978b9b161519bc94c7
George Carvalho <geocarvalho91@gmail.com>
2021-08-17 22:13:08 +0400

    Just add code to select female samples

--- epismoker/create_sample_sheet.py
+++ epismoker/create_sample_sheet.py
@@ -4,7 +4,7 @@ import pandas as pd
 # see example in website
 # http://htmlpreview.github.io/?https://github.com/sailalithabollepalli/EpiSmokEr/blob/master/vignettes/epismoker.html
 # open pheno data
-ss = pd.read_csv("GSE51032_all_phenotype.csv")
+ss = pd.read_csv("GSE51032/GSE51032_all_phenotype.csv")
 # Just select samples from breast cancer and normal
 ss["cancer type (icd-10):ch1"] = ss["cancer type (icd-10):ch1"].replace(
     np.nan, "normal", regex=True)
@@ -18,9 +18,11 @@ ss_filter["Age"] = ss_filter["age:ch1"].astype(int)
 # create gender and sex columns
 ss_filter["Gender"] = ss_filter["gender:ch1"].replace({"M": "m", "F": "f"})
 ss_filter["sex"] = ss_filter["gender:ch1"].replace({"M": 1, "F": 2})
+# just select female
+ss_filter = ss_filter[ss_filter["Gender"] == "f"]
 # create slide and array columns
 ss_filter["Slide"] = ss_filter.apply(lambda sample: sample["title"].split("_")[0], axis=1)
 ss_filter["Array"] = ss_filter.apply(lambda sample: sample["title"].split("_")[1], axis=1)
 # select only the columns created and create output csv file
 output = ss_filter[["", "Age", "Gender", "sex", "Slide", "Array"]]
-output.to_csv("idat/normal_mama/samplesheet_GSE109381_simple.csv", index=False)
+output.to_csv("GSE51032/idat/normal_mama/samplesheet_GSE109381_simple.csv", index=False)
