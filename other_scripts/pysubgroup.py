a8403796e188a0c2a80aa1e101b8bc05f93075ac
George Carvalho <geocarvalho91@gmail.com>
2021-08-21 11:14:29 +0400

    Update with all code

--- other_scripts/pysubgroup.py
+++ other_scripts/pysubgroup.py
@@ -4,3 +4,16 @@ import numpy as np
 
 data = "../download_geo/GSE51032/GSE51032_bvalues_filtered_cn_pheno_factor.csv"
 df = pd.read_csv(data)
+
+target = ps.BinaryTarget ('target', True)
+searchspace = ps.create_selectors(df, ignore=['target'])
+task = ps.SubgroupDiscoveryTask (
+    df, 
+    target, 
+    searchspace, 
+    result_set_size=5, 
+    depth=2, 
+    qf=ps.WRAccQF())
+result = ps.BeamSearch().execute(task)
+result_df = result.to_dataframe()
+restult_df.to_csv("../download_geo/GSE51032/GSE51032_beamsearch_result.csv", index=False)
