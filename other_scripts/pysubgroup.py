ecece4b8117e6c60b9846321f6c22b852f4ca33a
George Carvalho <geocarvalho91@gmail.com>
2021-08-28 15:39:20 +0400

    Update set_size=10 and depth=50

--- other_scripts/pysubgroup.py
+++ other_scripts/pysubgroup.py
@@ -4,6 +4,8 @@ import numpy as np
 
 data = "../download_geo/GSE51032/GSE51032_bvalues_filtered_cn_pheno_factor.csv"
 df = pd.read_csv(data)
+cols = [col for col in df.columns if col != "time_to_diagnosis_classes"]
+df = df[cols]
 
 target = ps.BinaryTarget('target', True)
 searchspace = ps.create_selectors(df, ignore=['target'])
@@ -11,9 +13,9 @@ task = ps.SubgroupDiscoveryTask (
     df, 
     target, 
     searchspace, 
-    result_set_size=5, 
-    depth=2, 
+    result_set_size=10, 
+    depth=50, 
     qf=ps.WRAccQF())
 result = ps.BeamSearch().execute(task)
 result_df = result.to_dataframe()
-restult_df.to_csv("../download_geo/GSE51032/GSE51032_beamsearch_result.csv", index=False)
+result_df.to_csv("../download_geo/GSE51032/GSE51032_beamsearch_result_dp50_sz10_nodiagnosis.csv", index=False)
