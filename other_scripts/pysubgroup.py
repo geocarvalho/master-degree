43695c5a12089157941dbb4d168679a68d27d8eb
George Carvalho <geocarvalho91@gmail.com>
2021-11-14 23:39:13 +0400

    (up) change size and input file

--- other_scripts/pysubgroup.py
+++ other_scripts/pysubgroup.py
@@ -2,7 +2,7 @@ import pysubgroup as ps
 import pandas as pd
 import numpy as np
 
-data = "../download_geo/GSE51032/GSE51032_bvalues_filtered_pheno_factor.csv"
+data = "../download_geo/GSE51032/GSE51032_bvalues_filtered_cn_pheno_factor.csv"
 df = pd.read_csv(data)
 cols = [col for col in df.columns if col != "time_to_diagnosis_classes"]
 df = df[cols]
@@ -13,9 +13,9 @@ task = ps.SubgroupDiscoveryTask (
     df, 
     target, 
     searchspace, 
-    result_set_size=50, 
+    result_set_size=20, 
     depth=100,
     qf=ps.WRAccQF())
 result = ps.BeamSearch(beam_width_adaptive=True).execute(task)
 result_df = result.to_dataframe()
-result_df.to_csv("../download_geo/GSE51032/GSE51032_beamsearch_result_dp100_sz50_nodiagnosis.csv", index=False)
+result_df.to_csv("../download_geo/GSE51032/GSE51032_beamsearch_result_dp100_sz20_nodiagnosis.csv", index=False)
